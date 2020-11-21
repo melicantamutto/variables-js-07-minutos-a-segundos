efd0ed3f47c027bb84719f24c6c663013a775a04
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 12:55:49 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Minutos a segundos')
+
+alert('Minutos a segundos')
+
+const minutos = prompt('Ingresa la cantidad de minutos');
+const segundos = (minutos * 60);
+
+alert(`${minutos} minutos son ${segundos} segundos`);
